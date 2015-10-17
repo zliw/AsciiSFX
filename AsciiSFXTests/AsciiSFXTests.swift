82820f8344db1cfd1cafaaf4c05e852c486a47d5
Martin Wilz <github@wilz.de>
2015-10-18 01:08:33 +0200

    test cases for integer parsing

--- AsciiSFXTests/AsciiSFXTests.swift
+++ AsciiSFXTests/AsciiSFXTests.swift
@@ -10,27 +10,21 @@ import XCTest
 @testable import AsciiSFX
 
 class AsciiSFXTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let parser = CommandParser()
+
+    func testParseInteger() {
+        let chars = Array("1000".characters)
+        let (value, index) = parser.parseInteger(chars)
+        assert(value == 1000)
+        assert(index == 4)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    func testParseInteger2() {
+        let chars = Array("".characters)
+        let (value, index) = parser.parseInteger(chars)
+        assert(value == 0)
+        assert(index == 0)
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
+
 }
