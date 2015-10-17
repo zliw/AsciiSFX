1bd023ff6ce3698fc4490a138370655b9dde9f74
Martin Wilz <github@wilz.de>
2015-10-17 12:32:00 +0200

    added parser class

---
+++ AsciiSFX/CommandParser.swift
@@ -0,0 +1,16 @@
+//
+//  CommandParser.swift
+//  AsciiSFX
+//
+//  Created by martin on 17/10/15.
+//  Copyright © 2015 martin. All rights reserved.
+//
+
+import Foundation
+
+class CommandParser {
+    func parse(command:NSString) -> Bool{
+        print(command)
+        return false
+    }
+}
--- AsciiSFX/ViewController.swift
+++ AsciiSFX/ViewController.swift
@@ -11,16 +11,20 @@ import Cocoa
 class ViewController: NSViewController {
     @IBOutlet var textField:NSTextField?
     @IBOutlet var playButton:NSButton?
+    let parser = CommandParser()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
 
     @IBAction func play(sender:NSObject?) {
-        playButton?.enabled = false
-        
+        let command = textField?.stringValue
+        if ((command != nil && command?.lengthOfBytesUsingEncoding(NSASCIIStringEncoding) > 0)) {
+            let valid = parser.parse(command!)
+            if (valid) {
+                playButton?.enabled = false
+            }
+        }
     }
 }
 
