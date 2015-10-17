af6c5d106c8fd025781ab012739986272a52a3bb
Martin Wilz <github@wilz.de>
2015-10-17 12:03:41 +0200

    autogenerated project

---
+++ AsciiSFX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AsciiSFX
+//
+//  Created by martin on 17/10/15.
+//  Copyright © 2015 martin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AsciiSFX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AsciiSFX
+//
+//  Created by martin on 17/10/15.
+//  Copyright © 2015 martin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
